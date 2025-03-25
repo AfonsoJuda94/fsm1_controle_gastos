8599c487e0efb0b9f4130cdf46b3a01cccd66db9
Your Judah <jd002.trbl7@gmail.com>
2025-03-25 01:05:00 -0300

    Criação do componente login, criação da função de fazer login no front usando o axios

---
+++ app-gastos-completo/front/src/components/Login.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import api from "../api";
+
+export default function Login(){
+    
+    const [user,setUser] = useState('');
+    const [password,setPassword] = useState('');
+
+
+    //Função para logar -> Ela verifica se a senha bate com a senha do banco
+    const logar = async (e) =>{
+        e.preventDefault()
+        try{
+            const response = await api.post(`/login:${user}`,{
+                "nome": `${user}`,
+                "senha": `${password}`
+            })
+            alert("Login sucesso")
+        }catch(error){
+            console.log(error)
+            alert("Login inválido!")
+        }
+    }
+    return(
+    <form onSubmit={logar}>
+        <label htmlFor="nome">Usuário:</label>
+        <input type="text" id="nome" onChange={ e => setUser(e.target.value)} required/>
+        <label htmlFor="senha">Senha:</label>
+        <input type="password" name="senha" id="senha" onChange={ e => setPassword(e.target.value)} required />
+        <button type="submit">Login</button>
+    </form>
+)
+}
