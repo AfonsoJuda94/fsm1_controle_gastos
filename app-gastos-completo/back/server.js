e24cf9a765fa8d57d997595288bccd98aaa09185
Your Judah <jd002.trbl7@gmail.com>
2025-03-26 00:11:47 -0300

    download de dotenv, configuração do JWT_SECRET, resolução do problema para gerar token e continuar o processo de login

--- app-gastos-completo/back/server.js
+++ app-gastos-completo/back/server.js
@@ -1,5 +1,8 @@
 const express = require("express"); // Importa o express
 const app = express(); //instancia o express
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
 
 const { PrismaClient } = require("@prisma/client"); //DIFERENTE
 const prisma = new PrismaClient(); //DIFERENTE
@@ -40,9 +43,9 @@ app.get("/gastos/:id", async (req, res) => {
 // Rota para adicionar um novo gasto
 app.post("/gastos", async (req, res) => {
   try {
-    const { descricao, valor, categoria, data } = req.body;
+    const { descricao, valor, categoria, data, usuario } = req.body;
     const novoGasto = await prisma.gasto.create({
-      data: { descricao, valor: parseFloat(valor), categoria, data: new Date(data) },
+      data: { descricao, valor: parseFloat(valor), categoria,usuario, data: new Date(data) },
     });
     res.status(201).json(novoGasto);
   } catch (error) {
@@ -80,47 +83,104 @@ app.get('/usuarios', async (req, res)=>{
   }
 })
 // Listar usuário específico
-app.get('/usuarios/:id', async (req,res)=>{
-  try{
-    const {id} = req.params;
-    const usuario = await prisma.usuario.findUnique({where:{id}})
-    if(!usuario){
-      return res.status(404).json('Usuário não encontrado')
-    }
-    res.json(usuario)
-  }catch(error){
-    res.status(500).json(error)
-  }
-})
-// Registrar usuário -> Funciona
+// app.get('/usuarios/:id', async (req,res)=>{
+//   try{
+//     const {id} = req.params;
+//     const usuario = await prisma.usuario.findUnique({where:{id}})
+//     if(!usuario){
+//       return res.status(404).json('Usuário não encontrado')
+//     }
+//     res.json(usuario)
+//   }catch(error){
+//     res.status(500).json(error)
+//   }
+// })
+// Registrar usuário -> Está funcionando
 app.post('/usuarios', async (req,res) => {
   try{
     const {nome, senha} = req.body;
-    const novo_usuario = await prisma.usuario.create({
-      data: {nome, senha}
-    })
-    res.json(novo_usuario)
-  }catch(error){
-    res.status(500).json('Erro ao registar usuário')
+    const usuario_existente = await prisma.usuario.findUnique({where:{nome}})
+    // console.log(usuario_existente)
+    if(usuario_existente){
+      return res.status(400).json({error:'Usuário já existente'})
+    }
+    else{
+      const hashSenha = await bcrypt.hash(senha,10)
+      console.log(hashSenha)
+      const novo_usuario = await prisma.usuario.create({
+      data: {nome, senha: hashSenha}
+      })
+      return res.json('Usuário registrado com sucesso')
+    }
+    }catch(error){
+    return res.status(500).json('Erro ao registar usuário')
+    }
+    
+})
+// Deletar usuário -> Está funcionando
+app.delete('/usuarios', async (req,res) =>{
+  try{
+    const {nome} = req.body;
+    console.log(nome)
+    const usuario_deletado = await prisma.usuario.findUnique({where:{nome}})
+    if(!usuario_deletado){
+      return res.status(404).json('Usuário não contrado')
+    }else{
+      await prisma.usuario.delete({where:{nome}})
+      return res.json('Usuário removido com sucesso')
+    }
+
+  }catch(err){
+    return res.status(500).json(err)
+  }
+})
+//Login --> Com problemas
+app.post('/login', async (req,res) =>{
+  try{
+    const {nome, senha} = req.body;
+    const usuario_bd = await prisma.usuario.findUnique({where:{nome}})
+    if(usuario_bd == null){
+      return res.status(404).json('Usuário não encontrado')
+    }else{
+      const senha_valida = await bcrypt.compare(senha, usuario_bd.senha);
+      if(!senha_valida){
+        return res.status(400).json('Senha inválida')
+      }
+      else{
+        // console.log("AAAAAAAAAAAAAAAAAAAAA") -> Estava tendo problemas aqui
+        console.log("JWT_SECRET:", process.env.JWT_SECRET);
+        const token = jwt.sign(
+          {nome: usuario_bd.nome }, process.env.JWT_SECRET,{expiresIn:'1h'}
+        )
+        // console.log("BBBBBBBBBBBBBBBBBBBBBBB")
+        return res.json({
+          token, 
+          usuario: {
+            nome: usuario_bd.nome
+          }
+        });
+      }
+    }
+  }catch(err){
+    return res.json({'erro':err})
   }
 })
 
-//Login --> Funcionando
-app.get('/login:nome', async (req,res) =>{
-  try{
-    const {nome,senha} = req.body;
-    const email = await prisma.usuario.findUnique({where:{nome}})
-    if(!email){
-      return res.status(404).json('Usuário não encontrado')
-    }
-    if(senha !== email.senha){
-      return res.status(500).json("Acesso negado!")
-    }
-    res.json(`Bem vindo, ${nome}!`)
-  }catch{
-    res.status(500).json('Erro no login. Verifique suas credenciais')
-  }
-})
+// Middleware de autenticação
+
+const autenticarToken = (req,res,next) =>{
+  const authHeader = req.headers['authorization']
+  const toke = authHeader && authHeader.split(' '[1])
+  if(token == null){
+    return res.sendStatus(401);
+  } 
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403)
+    req.nome = user;
+    next()
+  })
+}
+
 
 // Inicia o servidor
 app.listen(PORT, () => {
